ba74a2482c434d043c812dcb027a5f61983a6a7e
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2023-04-27 00:48:39 +0530

    Partially implement step() and _take_action()

--- gym_stock_trading/envs/stock_trading_env.py
+++ gym_stock_trading/envs/stock_trading_env.py
@@ -20,6 +20,9 @@ class StockTradingEnv(gym.Env):
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
+        # Current time instant
+        self.timestamp = 0
+
     def _get_obs(self):
         # Retrieve the observation space at any step
         pass
@@ -28,9 +31,53 @@ class StockTradingEnv(gym.Env):
         # Retrieve auxiliary information about the current state
         pass
 
+    def _get_reward(self):
+        # Compute the reward for performing a particular action
+        pass
+
+    def _take_action(self, action):
+        # Takes an action in the current state
+        
+        # Hold
+        if action[0] == 0:
+            return
+
+        # Buy
+        elif action[0] == 1:
+            if self.current_price * action[1] > self.cash_in_hand:
+                raise ValueError('Insufficient money')
+
+            self.shares_held += action[1]
+            self.cash_in_hand -= self.current_price * action[1]
+
+        # Sell
+        else:
+            if action[1] > self.shares_held:
+                raise ValueError('Insufficient stocks')
+
+            self.shares_held -= action[1]
+            self.cash_in_hand += self.current_price * action[1]
+
     def step(self, action):
         # Execute one time step in the environment and return the updated state, reward, termination and auxiliar information
-        pass
+        
+        # Check if the action is valid
+        if not self.action_space.contains(action):
+            raise ValueError('Invalid action')
+
+        self._take_action(action)
+        self.timestamp += 1
+        self.current_price = self.stock_prices[self.timestamp]
+        self.net_worth = self.cash_in_hand + self.shares_held * self.current_price 
+
+        observation = self._get_obs()
+        reward = self._get_reward()
+        terminated = True if self.timestamp >= len(self.df) else False
+        truncated = False
+        info = self._get_info()
+
+        return observation, reward, terminated, truncated, info
+
 
     def reset(self):
         # Reset the environment to its initial state
